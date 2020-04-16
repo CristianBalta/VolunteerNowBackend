88964c3311d4cede3b829b92a298d88a1cd41228
LuciBurcu <luciburcu@gmail.com>
2020-04-16 12:58:51 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+**Install the following dependencies**
+
+How to install? (if needed)
+
+
+Tools -> NuGET Package Manager -> Manage NuGET Package Manager Console -> Browse -> Search for it -> Install 
+
+#
+Microsoft.EntityFrameworkCore
+
+Microsoft.AspNetCore.Cors
+
+#
+For Mongo:
+
+Tools -> NuGET Package Manager -> Package Manager Console -> Type this: Install-Package MongoDB.Driver -Version 2.10.2
+
+#
+
+**MONGO INSTRUCTIONS WEBSITE**
+
+https://docs.mongodb.com/manual/reference/method/js-database/
+
+
+#
+
+**GOOD TUTORIAL WITH MONGO AND ASP**
+
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mongo-app?view=aspnetcore-3.1&tabs=visual-studio
---
+++ backendapi/backendapi/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using backendapi.Models;
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using backendapi.Services;
+
+namespace backendapi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.Configure<BookstoreDatabaseSettings>(Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
+
+            services.AddSingleton<IBookstoreDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
+
+            services.AddSingleton<BookService>();
+            services.AddCors(options => options.AddPolicy("AllowCors", builder =>
+            {
+                builder.WithOrigins("http:localhost:3000").WithMethods("GET", "PUT", "POST", "DELETE")
+                    .AllowAnyHeader().SetIsOriginAllowed((host) => true);
+            }));
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("AllowCors");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
