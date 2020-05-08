a89272572efe7731aca91ac3d0da43a2a34486f8
Andrei <Muiedinamo97>
2020-05-08 20:15:14 +0300

    volunteer can assign needs

--- backendapi/backendapi/Controllers/NeedsController.cs
+++ backendapi/backendapi/Controllers/NeedsController.cs
@@ -124,9 +124,6 @@ namespace backendapi.Controllers
                     Description = need.Description,
                     Date = need.Date,
                     Title = need.Title
-               
-
-
                 };
                 ListOfAssigned.Add(NeedDTO);
             }
